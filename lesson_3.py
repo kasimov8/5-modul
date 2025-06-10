42ce65eb3d923e231546cba902cde792fcaaddae
Kasimov <khasanboykasimov7@gmail.com>
2025-06-10 17:47:05 +0500

    dalete one import

--- lesson_3.py
+++ lesson_3.py
@@ -10,7 +10,6 @@ from os import getenv
 
 
 from aiogram import Bot, Dispatcher, html, F
-from aiogram.client import bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
